1312f8edee919963e8af465195c200f9762d9e29
Daniel Rodrigues <61955775+dxnielsr@users.noreply.github.com>
2020-11-18 06:21:37 -0400

    determinante 2x2 ou 3x3 inicio de codigo

---
+++ script.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main () {
+    int l = 0, c = 0, escolha = 0;
+    int A[2][2], B[3][3];
+    printf("Determinante de uma matriz");
+
+    printf("voce quer calcular o determinante de uma matriz 2x2 ou 3x3 ?");
+    printf("[2] - 2x2 [3] 3x3");
+    scanf("%d", &escolha);
+
+    switch (escolha)
+    {
+    case 2:
+        
+        break;
+    
+    case 3:
+
+        break;
+
+    default:
+        break;
+    }
+
+    printf("sequencia de scans: \n");
+    printf("a11, a12, a13, a21, a22, a23, a31, a32, a33");
+
+    
+
+    return 0;
+}
